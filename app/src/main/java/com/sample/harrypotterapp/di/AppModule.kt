3e01868db66025a14e79776c289c30501ada308d
harsh <harshitgupta.091995@gmail.com>
2023-11-04 21:58:08 +0530

    Update Module Dependencies

--- app/src/main/java/com/sample/harrypotterapp/di/AppModule.kt
+++ app/src/main/java/com/sample/harrypotterapp/di/AppModule.kt
@@ -11,31 +11,50 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.Dispatchers
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-    private val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
+    @Provides
+    @Singleton
+    fun providesMoshiAdapterFactory() = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
 
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit =
-        Retrofit.Builder()
-            .baseUrl(BuildConfig.API_BASE_URL)
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .build()
+    fun provideOkHttpClient(
+    ): OkHttpClient {
+        val okHttpClient = OkHttpClient.Builder()
+        if (BuildConfig.DEBUG) {
+            okHttpClient.addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+        }
+        okHttpClient.callTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
+            .readTimeout(15, TimeUnit.SECONDS)
+        return okHttpClient.build()
+    }
 
     @Provides
     @Singleton
-    fun provideCharacterApi(retrofit: Retrofit): CharactersApi =
-        retrofit.create(CharactersApi::class.java)
+    fun provideCharacterApi(okHttpClient: OkHttpClient, moshi: Moshi): CharactersApi = Retrofit.Builder()
+        .baseUrl(BuildConfig.API_BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .build()
+        .create(CharactersApi::class.java)
 
     @Provides
     @Singleton
-    fun provideCharacterRepository(api: CharactersApi): CharactersRepository = CharactersRepositoryImpl(api)
+    fun provideCharacterRepository(api: CharactersApi): CharactersRepository =
+        CharactersRepositoryImpl(api)
 
     @Provides
     @Singleton
